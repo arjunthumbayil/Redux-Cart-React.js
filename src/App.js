e299ce67a83082629e674c3e3c2ade50fdff5e20
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-12-02 18:23:48 +0530

    seperated reducer to different file

--- src/App.js
+++ src/App.js
@@ -5,28 +5,12 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
-
 import { createStore } from "redux";
 import { INCREASE, DECREASE } from "./action";
 
+import reducer from "./reducer";
 //initial store
 const initialStore = { count: 78, name: "Ravi" };
-
-function reducer(state, action) {
-  if (action.type === DECREASE) {
-    return { ...state, count: state.count - 1 };
-  }
-  if (action.type === INCREASE) {
-    return { ...state, count: state.count + 1 };
-  }
-  if (action.type === "RESET") {
-    return { ...state, count: 0 };
-  }
-  if (action.type === "CHANGE_NAME") {
-    return { ...state, name: "Kumar" };
-  }
-  return state;
-}
 
 const store = createStore(reducer, initialStore);
 console.log(store.getState());
