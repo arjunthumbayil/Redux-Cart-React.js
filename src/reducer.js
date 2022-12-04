2a4b29316a4af84a41a65994c1457fae9cf48d94
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-12-04 07:08:12 +0530

    refactored decrease reducer

--- src/reducer.js
+++ src/reducer.js
@@ -17,19 +17,13 @@ function reducer(state, action) {
     };
   }
   if (action.type === DECREASE) {
-    let tempCart = [];
-    if (action.payload.amount === 1) {
-      tempCart = state.cart.filter(
-        (cartItem) => cartItem.id !== action.payload.id
-      );
-    } else {
-      tempCart = state.cart.map((cartItem) => {
-        if (cartItem.id === action.payload.id) {
-          return { ...cartItem, amount: cartItem.amount - 1 };
-        }
-        return cartItem;
-      });
-    }
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        return { ...cartItem, amount: cartItem.amount - 1 };
+      }
+      return cartItem;
+    });
+
     return {
       ...state,
       cart: tempCart,
